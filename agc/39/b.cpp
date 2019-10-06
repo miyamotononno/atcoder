5f5955aa139ae2df52dd1e23dacf9ffac8c7054c
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2019-10-06 20:09:36 +0900

    solve by warshall

--- agc/39/b.cpp
+++ agc/39/b.cpp
@@ -10,30 +10,31 @@
 #include <cmath>
 #include <unordered_map>
 #define rep(i,n) for (int i=0; i<n;++i)
-#define rep_down(i,n) for (int i=n-1; i>=0;--i)
 #define ALL(a)  (a).begin(),(a).end()
 typedef long long ll;
 using namespace std;
-const ll MOD = 1000000007LL;
 const int INF = 1000000007;
 int N;
 vector<int> G[202];
 int color[200];
-vector<int> F;
+int L[201];
+int d[201][201];
 
-int dfs(int v, int c){
-  color[v] = c; //頂点vをcで塗る
-  int cnt = 0;
+bool dfs(int v, int c){
+  color[v] = c;
   rep(i, G[v].size()){
-    if (!color[G[v][i]]){
-      if(color[G[v][i]]==c) return -1;
-      cnt++;
-    }
-    //隣接している頂点がまだ塗られていないのなら-cで塗る
-    if(color[G[v][i]]==0 && !dfs(G[v][i], -c)) return -1;
+    if(color[G[v][i]]==c) return false;
+    if(color[G[v][i]]==0 && !dfs(G[v][i], -c)) return false;
   }
-  //全ての頂点が塗れたらtrue
-  return cnt;
+  return true;
+}
+
+void warshall_floyd() {
+  rep(k, N){
+    rep(i, N){
+      rep(j, N) d[i][j] =min(d[i][j], d[i][k]+d[k][j]);
+    }
+  }
 }
 
 int main() {
@@ -44,22 +45,31 @@ int main() {
   cin >> N;
   char a;
   rep(i, N){
-    for(int j=i+1; j<N; j++){
+    rep(j, N){
       cin >> a;
-      if (a == '1') G[i].push_back(j); // 有向グラフにする
-    }
-  }
-  int ans = 0;
-  rep(i, N){
-    if(color[i]==0){
-      int cnt = dfs(i, 1);
-      if (cnt < 0){
-        cout << -1 << endl;
-        return 0;
+      if (a == '1'){
+        G[i].push_back(j);
+        d[i][j] = 1;
+      } else {
+        d[i][j] = INF;
       }
     }
   }
 
-  cout << 2 << "\n";
+  if (!dfs(0, 1)){
+    cout << -1 << endl;
+    return 0;
+  }
+
+  warshall_floyd();
+  int ans = 0;
+  rep(i, N){
+    rep(j, N) {
+      if (i==j) continue;
+      ans = max(ans, d[i][j]);
+    }
+  }
+
+  cout << ans+1 << "\n";
   return 0;
 }
