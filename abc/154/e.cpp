2c638e2b9b74fc75b21900e94d3b9a0a8ee3b6f7
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2020-02-10 23:39:14 +0900

    solve abc154 e

--- abc/154/e.cpp
+++ abc/154/e.cpp
@@ -17,7 +17,31 @@ const ll MOD = 1e9+7LL;
 const int INF = 2e9;
 string S;
 int K;
-ll DP[105][4]; // 右からn桁目までで０でないものを使った数字の個数。
+
+// Nは桁数, countは0でないものの個数。これらが決まったときにあり得る数の個数(n C countで求まる)
+ll combi(ll N, int count) {
+  if (count == 1) return N*9ll;
+  if (count == 2) return 81ll*N*(N-1)/2;
+  if (count == 3) return 729ll*N*(N-1)*(N-2)/6;
+  else return 0ll;
+}
+// i 桁目以降について、0 以外の値を残り K 個使用可能という状況を考える。
+// このとき i 桁目までの部分が「等しい」か「strict に小さくなっている」かを smaller フラグによって分岐する。
+ll dfs(int idx, int k, bool smaller) {
+  if (k==0) return 1ll; // 以下全部0
+  if (smaller) return combi((ll)S.size()-idx, k);
+  if (idx==S.size()) {
+    if (k==0) return 1ll;
+    else return 0ll;
+  }
+  if (S[idx]=='0') return dfs(idx+1, k, false);
+
+  ll zero_count = dfs(idx+1, k, true);
+  ll between_count = ll(S[idx] - '1') * dfs(idx+1, k-1, true);
+  ll icchi_count = dfs(idx+1, k-1, false);
+  return zero_count+between_count+icchi_count;
+}
+
 int main() {
   cin.sync_with_stdio(false);
   cin.tie(0);
@@ -26,26 +50,6 @@ int main() {
   cin >> S;
   cin >> K;
   
-  for (int i=S.size()-1; i>=0; i--) {
-    int n =S[i]-'0';
-    if (i==S.size()-1) {
-      DP[i][0] = 1;
-      DP[i][1] = n;
-      continue;
-    }
-
-    rep(j, n+1) {
-      if (j==0) {
-        rep(k, K) DP[i][k] += DP[i+1][k];
-      } else {
-        rep(k, K) DP[i][k+1] += DP[i+1][k]*9+DP[i+1][k+1]*10;
-      }
-    }
-  }
-
-  rep(i, K+1) {
-    cout << DP[0][i] << "\n";
-  }
-  // cout << DP[0][K] << "\n";
+  cout <<  dfs(0, K, false) << "\n";
   return 0;
 }
