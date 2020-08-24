e3b7c3658e5711d7d76779918ea0b800552a283c
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2020-08-24 18:29:52 +0900

    solve abc110

---
+++ abc/heisei/110/d.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <random>
+#include <bitset>
+#include <queue>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+#include <map>
+#define INCANT cin.tie(0), cout.tie(0), ios::sync_with_stdio(0), cout << fixed << setprecision(20);
+#define rep(i,n) for (int i=0; i<n;++i)
+#define ALL(a)  (a).begin(),(a).end()
+typedef long long ll;
+using namespace std;
+const ll MOD = 1e9+7LL;
+const int MAX = 1e6;
+int N, M;
+ll fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+ll COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+map<int, int> prime_factor(int n) {
+  map<int, int> ret;
+  for(int i = 2; i * i <= n; i++) {
+    while(n % i == 0) {
+      ret[i]++;
+      n /= i;
+    }
+  }
+  if(n != 1) ret[n] = 1;
+  return ret;
+}
+
+
+
+int main() {
+  INCANT;
+  cin >> N >> M;
+  if (M==1) {
+    cout << 1 << endl;
+    return 0;
+  }
+  COMinit();
+  map<int, int> pf = prime_factor(M);
+
+  ll ans = 1ll;
+  for (auto p: pf) {
+    ans*=COM(p.second+N-1, p.second);
+    ans%=MOD;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
