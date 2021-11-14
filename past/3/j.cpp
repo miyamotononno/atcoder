b58339491eb71f11f8b5d45a0407b9444bae39c4
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2021-11-15 00:27:42 +0900

    solve past3 j again using g++

--- past/3/j.cpp
+++ past/3/j.cpp
@@ -1,52 +1,34 @@
-#include <algorithm>
-#include <iostream>
-#include <iomanip>
-#include <cassert>
-#include <cstring>
-#include <string>
-#include <vector>
-#include <random>
-#include <bitset>
-#include <queue>
-#include <cmath>
-#include <unordered_map>
-#include <set>
-#include <map>
-#include <stack>
-#define INCANT cin.tie(0), cout.tie(0), ios::sync_with_stdio(0), cout << fixed << setprecision(20);
-#define rep(i,n) for (int i=0; i<n;++i)
-#define ALL(a)  (a).begin(),(a).end()
-#define PI 3.14159265358979
-typedef long long ll;
+#include <bits/stdc++.h>
 using namespace std;
-const ll MOD = 1e9+7LL;
-const int INF = 2e9;
-int N, M;
-int a[300005];
-int top[100005];
-int Ans[100005];
 
-int bs(int x) { // 左の方が大きい
-  int left = -1;
-  int right = N;
-  while(right-left>1) {
-    int mid = (left+right)/2;
-    if (top[mid]>=x) left = mid;
-    else right = mid;
+/*
+generate関数
+https://cpprefjp.github.io/reference/algorithm/generate.html
+*/
+
+void solve(const int& n, const int& m, int a[], int outputs[]) {
+  int maxSushi[n];
+  generate(maxSushi, maxSushi+n, [](){return 0;});
+  for (int i=0; i<m; i++) {
+    int idx = upper_bound(maxSushi, maxSushi+n, -a[i]) - maxSushi;
+    if (idx == n) {
+      outputs[i] = -1;
+      continue;
+    }
+    maxSushi[idx] = -a[i];
+    outputs[i] = idx+1;
   }
-  return right;
+  return;
 }
 
 int main() {
-  INCANT;
+  int N, M;
   cin >> N >> M;
-  rep(i, M) cin >> a[i];
-  fill(top, top+N+1, -1);
-  rep(i, M) {
-    int idx = bs(a[i]);
-    top[idx] = a[i];
-    int ans = idx==N?-1:idx+1;
-    cout << ans << "\n";
+  int a[M];
+  for (int i=0; i<M; i++) cin >> a[i];
+  int outputs[M];
+  solve(N,M,a, outputs);
+  for (int i=0; i<M; i++) {
+    cout << outputs[i] << endl;
   }
-  return 0;
 }
